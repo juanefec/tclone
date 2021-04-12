b7a9f499baa7d3021fc601caddd228d383678d89
juancruzbri <brizuela.juancruz@gmail.com>
2021-04-12 17:32:34 -0300

    agregado boton de menu y componente reutilizable

---
+++ src/components/AppDropdownContent.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div v-if="active">
+    <slot/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppDropdownContent',
+    inject: ['sharedState'],
+    computed: {
+      active () {
+        return this.sharedState.active
+      }
+    }
+  }
+</script>
